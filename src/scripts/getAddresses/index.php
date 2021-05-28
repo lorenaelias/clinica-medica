56ba833f153c6bab5b63d1e6c808ef1fd8701598
Lorena <lorena_dasilvaelias@hotmail.com>
2021-05-28 16:15:08 -0300

    listagem de enderecos ok

---
+++ src/scripts/getAddresses/index.php
@@ -0,0 +1,40 @@
+<?php
+
+require "../../../../conexaoMysql.php";
+$pdo = mysqlConnect();
+
+try {
+
+  $sql = <<<SQL
+  SELECT logradouro, estado, cidade, cep
+  FROM ENDERECO_TF
+  SQL;
+
+  $stmt = $pdo->prepare($sql);
+} catch (Exception $e) {
+  exit('Ocorreu uma falha: ' . $e->getMessage());
+}
+
+class Endereco
+{
+  public $logradouro;
+  public $estado;
+  public $cidade;
+  public $cep;
+
+  function __construct($logradouro, $estado, $cidade, $cep)
+  {
+    $this->logradouro = $logradouro;
+    $this->estado = $estado; 
+    $this->cidade = $cidade;
+    $this->cep = $cep;
+  }
+}
+
+$stmt->execute();
+
+$enderecos = array();
+while ($row = $stmt->fetch()) {
+  array_push($enderecos, new Endereco($row['logradouro'], $row['estado'], $row['cidade'], $row['cep']));
+}
+echo json_encode($enderecos);
