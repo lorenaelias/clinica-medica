db34357f606e3fd302b7eddd75e088fde684e4c4
Lorena <lorena_dasilvaelias@hotmail.com>
2021-05-29 22:54:39 -0300

    script de verificacao de médico

---
+++ src/scripts/isDoc/index.php
@@ -0,0 +1,39 @@
+<?php
+
+require "../../../../conexaoMysql.php";
+require_once "../autenticacao.php";
+
+session_start();
+
+$pdo = mysqlConnect();
+
+class RequestResponse{
+    public $success;
+
+    function __construct($success)
+    {
+        $this->success = $success;
+    }
+}
+
+$medico = false;
+
+$sql = <<<SQL
+SELECT crm
+FROM MEDICO_TF
+WHERE codigo = ?
+SQL;
+
+try {
+    $stmt = $pdo->prepare($sql);
+    $stmt->execute([$_SESSION['cod']]);
+    $row = $stmt->fetch();
+
+    if ($row)
+        $medico = true;
+} 
+catch (Exception $e) {
+    exit('Falha inesperada: ' . $e->getMessage());
+}
+
+echo json_encode(new RequestResponse($medico));
