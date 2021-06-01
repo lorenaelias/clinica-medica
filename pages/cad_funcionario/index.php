6329d015ef6cc6482cbafbef22c59d88574df4ed
Lorena <lorena_dasilvaelias@hotmail.com>
2021-05-31 21:43:29 -0300

    tirei  /> troquei por >

--- pages/cad_funcionario/index.php
+++ pages/cad_funcionario/index.php
@@ -10,30 +10,30 @@ exitWhenNotLogged($pdo);
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta charset="UTF-8" >
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" >
   <link rel="shortcut icon" href="../../public/icons/Logo.png" type="image/png">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <link rel="preconnect" href="https://fonts.gstatic.com" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+  <link rel="preconnect" href="https://fonts.gstatic.com" >
   <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
-    rel="stylesheet" />
-  <link rel="stylesheet" href="../../styles/globalStyles.css" />
-  <link rel="stylesheet" href="../../styles/cad_funcionario.css" />
+    rel="stylesheet" >
+  <link rel="stylesheet" href="../../styles/globalStyles.css" >
+  <link rel="stylesheet" href="../../styles/cad_funcionario.css" >
   <title>DevHealth | Cadastrar funcionário</title>
 </head>
 
 <body class="cadFuncionario__container">
   <aside class="cadFuncionario__aside">
     <nav class="cadFuncionario__aside__icons">
-      <a href="../dashboard"><img src="../../public/icons/home.png" alt="home" /></a>
-      <a href="../cad_funcionario"><img src="../../public/icons/heart.png" alt="heart" /></a>
-      <a href="../cad_paciente"><img src="../../public/icons/peoples.png" alt="peoples" /></a>
-      <a href="../list_pacientes"><img src="../../public/icons/painel.png" alt="painel" /></a>
-      <a href="../list_funcionarios"><img src="../../public/icons/group.png" alt="group" /></a>
-      <a href="../list_endereco"><img src="../../public/icons/marker.png" alt="marker.png" /></a>
-      <a href="../list_agendamentos"><img src="../../public/icons/calendar.png" alt="calendar.png" /></a>
-      <a href="../list_meus_agendamentos" id="meusAgend" style="display: none;"><img src="../../public/icons/note.png" alt="note" /></a>
-      <a href="../../src/scripts/logout.php"><img src="../../public/icons/logout.png" alt="logout.png" /></a>
+      <a href="../dashboard"><img src="../../public/icons/home.png" alt="home" ></a>
+      <a href="../cad_funcionario"><img src="../../public/icons/heart.png" alt="heart" ></a>
+      <a href="../cad_paciente"><img src="../../public/icons/peoples.png" alt="peoples" ></a>
+      <a href="../list_pacientes"><img src="../../public/icons/painel.png" alt="painel" ></a>
+      <a href="../list_funcionarios"><img src="../../public/icons/group.png" alt="group" ></a>
+      <a href="../list_endereco"><img src="../../public/icons/marker.png" alt="marker.png" ></a>
+      <a href="../list_agendamentos"><img src="../../public/icons/calendar.png" alt="calendar.png" ></a>
+      <a href="../list_meus_agendamentos" id="meusAgend" style="display: none;"><img src="../../public/icons/note.png" alt="note" ></a>
+      <a href="../../src/scripts/logout.php"><img src="../../public/icons/logout.png" alt="logout.png" ></a>
     </nav>
   </aside>
   <div class="cadFuncionario__content">
--- pages/list_funcionarios/index.php
+++ pages/list_funcionarios/index.php
@@ -9,31 +9,31 @@ exitWhenNotLogged($pdo);
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta charset="UTF-8" >
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" >
     <link rel="shortcut icon" href="../../public/icons/Logo.png" type="image/png">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" >
+    <link rel="preconnect" href="https://fonts.gstatic.com" >
     <link
       href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap"
       rel="stylesheet"
-    />
-    <link rel="stylesheet" href="../../styles/globalStyles.css" />
-    <link rel="stylesheet" href="../../styles/list_funcionarios.css" />
+    >
+    <link rel="stylesheet" href="../../styles/globalStyles.css" >
+    <link rel="stylesheet" href="../../styles/list_funcionarios.css" >
     <title>DevHealth | Listar Funcionários</title>
   </head>
   <body class="listFuncionarios__container" onload="ifMedico(); buscaFuncionarios();">
         <aside class="listFuncionarios__aside">
             <nav class="listFuncionarios__aside__icons">
-                <a href="../dashboard"><img src="../../public/icons/home.png" alt="home" /></a>
-                <a href="../cad_funcionario"><img src="../../public/icons/heart.png" alt="heart" /></a>
-                <a href="../cad_paciente"><img src="../../public/icons/peoples.png" alt="peoples" /></a>
-                <a href="../list_pacientes"><img src="../../public/icons/painel.png" alt="painel" /></a>
-                <a href="../list_funcionarios"><img src="../../public/icons/group.png" alt="group" /></a>
-                <a href="../list_endereco"><img src="../../public/icons/marker.png" alt="marker.png" /></a>
-                <a href="../list_agendamentos"><img src="../../public/icons/calendar.png" alt="calendar.png" /></a>
-                <a href="../list_meus_agendamentos" id="meusAgend" style="display: none;"><img src="../../public/icons/note.png" alt="note" /></a>
-                <a href="../../src/scripts/logout.php"><img src="../../public/icons/logout.png" alt="logout.png" /></a>
+                <a href="../dashboard"><img src="../../public/icons/home.png" alt="home" ></a>
+                <a href="../cad_funcionario"><img src="../../public/icons/heart.png" alt="heart" ></a>
+                <a href="../cad_paciente"><img src="../../public/icons/peoples.png" alt="peoples" ></a>
+                <a href="../list_pacientes"><img src="../../public/icons/painel.png" alt="painel" ></a>
+                <a href="../list_funcionarios"><img src="../../public/icons/group.png" alt="group" ></a>
+                <a href="../list_endereco"><img src="../../public/icons/marker.png" alt="marker.png" ></a>
+                <a href="../list_agendamentos"><img src="../../public/icons/calendar.png" alt="calendar.png" ></a>
+                <a href="../list_meus_agendamentos" id="meusAgend" style="display: none;"><img src="../../public/icons/note.png" alt="note" ></a>
+                <a href="../../src/scripts/logout.php"><img src="../../public/icons/logout.png" alt="logout.png" ></a>
             </nav>
         </aside>
         <div class="listFuncionarios__content">
